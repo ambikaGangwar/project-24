2ab183bbc28e5f7f2f35b13caec25b656d42ebf3
ambikaGangwar <74405214+ambikaGangwar@users.noreply.github.com>
2020-12-11 18:00:12 +0530

    Update ground.js

--- ground.js
+++ ground.js
@@ -1,4 +1,4 @@
-   
+ 
 class Ground{
     constructor(x,y,width,height){
         var options={
@@ -8,11 +8,18 @@ class Ground{
         this.body = Bodies.rectangle(x,y,width,height,options)
         this.width = width
         this.height = height
+        this.x=x;
+        this.y=y;
         World.add(world,this.body)
        }
        display(){
-           rectMode(CENTER)
-           rect(this.body.position.x,this.body.position.y,this.width,this.height)
+           var groundPos=this.body.position;
+           push()
+           translate(groundPos.x,groundPos.y)
+           rectMode(CENTER);
+           fill(255,255,0);
+           rect(0,0,this.width,this.height)
+           pop();
        }
    
    }
